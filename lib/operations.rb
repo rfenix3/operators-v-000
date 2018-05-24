a50c2ab53bf0e2ddbf69abe82183894ec139c2e4
Richard Fenix <richard.fenix@gmail.com>
2018-05-24 22:52:29 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,17 @@
+require 'pry'
+
 def unsafe?(speed)
-
+  if speed < 40 || speed > 60
+    TRUE
+  else
+    FALSE
+  end
 end
-
 
 
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60 ? TRUE : FALSE
 end
-	
+
 
 
